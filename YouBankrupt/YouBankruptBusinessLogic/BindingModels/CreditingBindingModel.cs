5dda48728c36830cb955f659ed3f9031624997aa
Jambo-TimDevelopment <jambo-timDevelopment@yandex.ru>
2021-04-07 18:09:39 +0400

    change business logic project config

---
+++ YouBankrupt/YouBankruptBusinessLogic/BindingModels/CreditingBindingModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace YouBankruptBusinessLogic.BindingModels
+{
+    public class Crediting
+    {
+        [DataMember]
+        public int? Id { get; set; }
+    }
+}
---
+++ YouBankrupt/YouBankruptBusinessLogic/BindingModels/PaymentBindingModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace YouBankruptBusinessLogic.BindingModels
+{
+    public class PaymentBindingModel
+    {
+        [DataMember]
+        public int? Id { get; set; }
+
+        [DataMember]
+        public int Sum { get; set; }
+
+        [DataMember]
+        public int? TransactionWithClientId { get; set; }
+    }
+}
