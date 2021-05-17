c2106204db0402e6e55048c9caf9cf92fc76ddb3
aleca <aleca2002@yandex.ru>
2021-05-17 19:50:40 +0300

    start develop final version

--- YouBankrupt/YouBankruptDatabase/Models/PurchasesCurrenceCurrence.cs
+++ YouBankrupt/YouBankruptDatabase/Models/PurchasesCurrenceCurrence.cs
@@ -10,8 +10,6 @@ namespace YouBankruptDatabaseImplements.Models
         public int Id { get; set; }
         public int PurchasesCurrenceId { get; set; }
         public int CurrenceId { get; set; }
-        [Required]
-        public int Count { get; set; }
         public virtual PurchasesCurrence PurchasesCurrense { get; set; }
         public virtual Currence Currence { get; set; }
     }
