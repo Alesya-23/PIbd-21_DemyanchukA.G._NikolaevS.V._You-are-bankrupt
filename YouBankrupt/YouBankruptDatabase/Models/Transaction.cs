c0308b17dbddea09b0fa1ca57230f31a83dc47be
aleca <aleca2002@yandex.ru>
2021-05-25 22:04:31 +0300

    fix

---
+++ YouBankrupt/YouBankruptDatabase/Models/Transaction.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace YouBankruptDatabaseImplements.Models
+{
+    public class Transaction
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public DateTime DateFrom { get; set; }
+
+        [Required]
+        public DateTime DateTo { get; set; }
+
+        public int CustomerId { get; set; }
+
+        public virtual Customer Customer { get; set; }
+
+        public int? CreditProgramId { get; set; }
+
+        public virtual CreditProgram CreditProgram { get; set; }
+
+        public int? CreditingId { get; set; }
+
+        public virtual Crediting Crediting { get; set; }
+    }
+}
